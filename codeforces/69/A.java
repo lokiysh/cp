dc833024dc42023a1d1c56d8a347805ee17186bf
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2011-10-25 01:30:00 +0530

    Add solution for problem `A - Young Physicist` Link: https://codeforces.com/contest/69/problem/A Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/69/submission/794041

--- codeforces/69/A.java
+++ codeforces/69/A.java
@@ -1,36 +1,19 @@
-/*
-********QUESTION DESCRIPTION*************
- 
- 
-* @author (codeKNIGHT) 
- */
 import java.util.*;
-import java.math.*;
-import java.io.*;
-public class physics
+public class Main
 {
-    public static void main(String args[])throws IOException
-    {
-        Scanner in=new Scanner(System.in);
-        PrintWriter out=new PrintWriter(System.out);
-        //FileReader f=new FileReader("C:\\users\\Lokesh\\Desktop\\input.txt");
-            //Scanner in=new Scanner(f);
-        int t=in.nextInt();
-        int i,xs=0,ys=0,zs=0;
-        String s;
-        for(i=0;i<t;i++)
-        {
-            xs+=in.nextInt();
-            ys+=in.nextInt();
-            zs+=in.nextInt();
-        }
-        if(xs==0&&ys==0&&zs==0)
-        s="YES";
-        else
-        s="NO";
-        out.println(s);
-        
-
-        out.flush();
-    } 
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+int n=in.nextInt();
+int i,j,x[]=new int[n],y[]=new int[n],z[]=new int[n],xs=0,ys=0,zs=0;
+for(i=0;i<n;i++)
+{
+xs+=in.nextInt();
+ys+=in.nextInt();
+zs+=in.nextInt();
+}
+if(xs==0&&ys==0&&zs==0)
+System.out.println("YES");
+else System.out.println("NO");
+}
 }
