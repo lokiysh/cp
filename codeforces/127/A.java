83882efa9195bafc6fb2750ca26b79ba9319372a
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2011-11-09 22:46:00 +0530

    Add solution for problem `A - Wasted Time` Link: https://codeforces.com/contest/127/problem/A Tags: geometry, *900 Ref: https://codeforces.com/contest/127/submission/845032

--- codeforces/127/A.java
+++ codeforces/127/A.java
@@ -1,43 +1,41 @@
-import java.util.Scanner;
-import java.io.OutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.InputStream;
 
-/**
- * Built using CHelper plug-in
- * Actual solution is at the top
- * @author codeKNIGHT
+/*
+********QUESTION DESCRIPTION*************
+ 
+ 
+* @author (codeKNIGHT) 
  */
-public class Main {
-	public static void main(String[] args) {
-		InputStream inputStream = System.in;
-		OutputStream outputStream = System.out;
-		Scanner in = new Scanner(inputStream);
-		PrintWriter out = new PrintWriter(outputStream);
-		TaskA solver = new TaskA();
-		solver.solve(1, in, out);
-		out.close();
-	}
-}
-
-class TaskA {
-	public void solve(int testNumber, Scanner in, PrintWriter out) {
-        int n=in.nextInt()  ;
-        int k=in.nextInt()  ;
-        int i,x[]=new int[n],y[]=new int[n];
+import java.util.*;
+import java.math.*;
+import java.io.*;
+public class a93
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner in=new Scanner(System.in);
+        PrintWriter out=new PrintWriter(System.out);
+        //FileReader f=new FileReader("C:\\users\\Lokesh\\Desktop\\input.txt");
+            //Scanner in=new Scanner(f);
+        //int t=in.nextInt();
+        int i;
+        String s;
+        int n,k;
         double d=0;
+        n=in.nextInt();
+        k=in.nextInt();
+        int x[]=new int[n];
+        int y[]=new int[n];
         for(i=0;i<n;i++)
         {
             x[i]=in.nextInt();
-            y[i]=in.nextInt()  ;
-
+            y[i]=in.nextInt();
         }
         for(i=0;i<n-1;i++)
         {
             d+=Math.sqrt(Math.pow((x[i]-x[i+1]),2)+Math.pow((y[i]-y[i+1]),2));
         }
         out.println((d*k)/50);
-	}
-}
 
+        out.flush();
+    } 
+}
