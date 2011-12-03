01897665a44e3c1bfcd71591ffb82df5db95be2b
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2011-12-03 22:09:00 +0530

    Add solution for problem `B - Unary` Link: https://codeforces.com/contest/133/problem/B Tags: implementation, *1200 Ref: https://codeforces.com/contest/133/submission/925660

--- codeforces/133/B.java
+++ codeforces/133/B.java
@@ -1,42 +1,54 @@
-import java.util.Scanner;
-import java.io.OutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.InputStream;
 
-/**
- * Built using CHelper plug-in
- * Actual solution is at the top
- * @author codeKNIGHT
+/*
+********QUESTION DESCRIPTION*************
+ 
+ 
+* @author (codeKNIGHT) 
  */
-public class Main {
-	public static void main(String[] args) {
-		InputStream inputStream = System.in;
-		OutputStream outputStream = System.out;
-		Scanner in = new Scanner(inputStream);
-		PrintWriter out = new PrintWriter(outputStream);
-		TaskB solver = new TaskB();
-		solver.solve(1, in, out);
-		out.close();
-	}
-}
-
-class TaskB {
-	public void solve(int testNumber, Scanner in, PrintWriter out) {
-        String s=in.next();
-        char a[]={'>','<','+','-','.',',','[',']'};
-        int i,j,mod=1000003;
+import java.util.*;
+import java.math.*;
+import java.io.*;
+public class b1
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner in=new Scanner(System.in);
+        PrintWriter out=new PrintWriter(System.out);
+        //FileReader f=new FileReader("C:\\users\\Lokesh\\Desktop\\input.txt");
+            //Scanner in=new Scanner(f);
+        //int t=in.nextInt();
+        int i,j;
+        String s=in.next(),ans="";
+        char a[]={'>','<','+','-','.',',','[',']'},ch;
+        long l=(s.length()-1)*4,m=0,t=1000003;
         long k=0;
+        BigInteger b=BigInteger.valueOf(1),mod=BigInteger.valueOf(1000003);
+       // System.out.println(1<<40);
         for(i=0;i<s.length();i++)
         {
-            char ch=s.charAt(i);
+            ch=s.charAt(i);
             for(j=0;j<8;j++)
-                if(ch==a[j])
-                    break;
-            k=(k*16)%mod;
-            k+=j+8;
+            {
+                if(a[j]==ch)
+                break;
+            }
+            ans+=Integer.toBinaryString(j+8);
         }
+       // System.out.println(ans);
+        for(i=ans.length()-1;i>=0;i--)
+        {
+            if(ans.charAt(i)=='1')
+            {
+                b=BigInteger.valueOf(1);
+                b=b.shiftLeft(ans.length()-1-i);
+                b=b.mod(mod);
+                m=b.longValue();
+                //System.out.println(d);
+                k=(k+m)%t;
+            }
+        }
+        //out.println(ans);
         out.println(k);
-	}
+        out.flush();
+    } 
 }
-
