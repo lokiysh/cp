66c31b1babea4781aa99874dffbb620430fa90eb
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-06-10 21:15:00 +0530

    Add solution for problem `A - Let's Watch Football` Link: https://codeforces.com/contest/195/problem/A Tags: binary search, brute force, math, *1000 Ref: https://codeforces.com/contest/195/submission/1776308

---
+++ codeforces/195/A.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		Scanner in = new Scanner(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+	public void solve(int testNumber, Scanner in, PrintWriter out) {
+        int a=in.nextInt(),b=in.nextInt(),c=in.nextInt();
+        out.println((int)Math.ceil((double )(c*a-c*b)/b));
+	}
+}
+
