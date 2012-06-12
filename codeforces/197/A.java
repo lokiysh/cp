09dec720add08202cd9b13d2371bedd15c7d54de
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-06-12 20:58:00 +0530

    Add solution for problem `A - Plate Game` Link: https://codeforces.com/contest/197/problem/A Tags: constructive algorithms, games, math, *1600 Ref: https://codeforces.com/contest/197/submission/1796631

---
+++ codeforces/197/A.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		Scanner in = new Scanner(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+	public void solve(int testNumber, Scanner in, PrintWriter out) {
+        int a=in.nextInt(),b=in.nextInt(),c=2*in.nextInt();
+        
+        if(a<c||b<c)
+            out.println("Second");
+        else out.println("First");
+	}
+}
+
