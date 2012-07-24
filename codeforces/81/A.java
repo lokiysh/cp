e7ebd760cf1ae479aa75f2cb2e2a7c8ffa9a1e30
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-07-25 02:24:00 +0530

    Add solution for problem `A - Plug-in` Link: https://codeforces.com/contest/81/problem/A Tags: implementation, *1400 Ref: https://codeforces.com/contest/81/submission/1937258

---
+++ codeforces/81/A.java
@@ -0,0 +1,70 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+	public void solve(int testNumber, InputReader in, PrintWriter out) {
+        String p=in.next();
+        StringBuffer s=new StringBuffer("");
+        int i;
+        for(i=0;i<p.length();i++)
+        {
+            if(s.length()==0)
+                s.append(p.charAt(i));
+            else if(s.charAt(s.length()-1)==p.charAt(i))
+            {
+                s.deleteCharAt(s.length()-1);
+                
+            }
+            else s.append(p.charAt(i));
+        }
+        out.println(s);
+	}
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    }
+
