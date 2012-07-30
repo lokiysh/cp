ec47096c9a4d0d6638f0b9940ff91379cc5a32ba
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-07-30 21:03:00 +0530

    Add solution for problem `A - System of Equations` Link: https://codeforces.com/contest/214/problem/A Tags: brute force, *800 Ref: https://codeforces.com/contest/214/submission/1953515

---
+++ codeforces/214/A.java
@@ -0,0 +1,73 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+	public void solve(int testNumber, InputReader in, PrintWriter out) {
+        int n=in.nextInt(),m=in.nextInt(),a,b,c=0;
+        int limit1=(int)Math.sqrt(n)+1;
+        int limit2=(int)Math.sqrt(m)+1;
+        for(a=0;a<limit1;a++)
+            for(b=0;b<limit2;b++)
+            {
+                if(a*a+b==n&&b*b+a==m)
+                    c++;
+            }
+        out.println(c);
+	}
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                String s;
+                if((s=in.readLine())!=null)
+                tokenizer=new StringTokenizer(s);
+                else return null;
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    public int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+
+}
+
