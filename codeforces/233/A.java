a797e00af91b572289a632f6b5c6e6c3023ce645
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-10-11 21:09:00 +0530

    Add solution for problem `A - Perfect Permutation` Link: https://codeforces.com/contest/233/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/233/submission/2340488

---
+++ codeforces/233/A.java
@@ -0,0 +1,72 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+	public void solve(int testNumber, InputReader in, PrintWriter out) {
+        int n=in.nextInt();
+        if(n%2==1)
+        {
+            out.println(-1);
+            return;
+        }
+        int i;
+        for(i=1;i<=n/2;i++)
+        {
+            out.print(2*i+" " );
+            out.print(2*i-1+" ");
+        }
+	}
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    public int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+
+}
+
