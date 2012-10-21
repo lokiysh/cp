388bdc877f7b52a00c66585e1cfbaaa477d9e0fb
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-10-21 12:43:00 +0530

    Add solution for problem `A - Boy or Girl` Link: https://codeforces.com/contest/236/problem/A Tags: brute force, implementation, strings, *800 Ref: https://codeforces.com/contest/236/submission/2395535

---
+++ codeforces/236/A.java
@@ -0,0 +1,68 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+	public void solve(int testNumber, InputReader in, PrintWriter out) {
+        boolean a[]=new boolean[26];
+        int i;
+        String s=in.next();
+        for(i=0;i<s.length();i++)
+            a[s.charAt(i)-'a']=true;
+        int c=0;
+        for(i=0;i<26;i++)
+            if(a[i])
+                c++;
+        if(c%2==0)
+            out.println("CHAT WITH HER!");
+        else out.println("IGNORE HIM!");
+	}
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+
+}
+
