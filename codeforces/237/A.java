df1d5e6a9d8d18ef1705a48de9c000c2f162b843
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-10-25 21:04:00 +0530

    Add solution for problem `A - Free Cash` Link: https://codeforces.com/contest/237/problem/A Tags: implementation, *1000 Ref: https://codeforces.com/contest/237/submission/2426583

---
+++ codeforces/237/A.java
@@ -0,0 +1,74 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+	public void solve(int testNumber, InputReader in, PrintWriter out) {
+        int n=in.nextInt();
+        int i;
+        int a[]=new int[2500];
+        for(i=0;i<n;i++)
+        {
+           int h=in.nextInt();
+            int m=in.nextInt();
+            a[h*60+m]++;
+        }
+        int min=0;
+        for(i=0;i<a.length;i++)
+            if(a[i]>min)
+                min=a[i];
+        out.println(min);
+	}
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    public int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+
+}
+
