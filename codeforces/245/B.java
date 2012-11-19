660779436140f7db3399de430686a0a5181c0744
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-11-19 22:14:00 +0530

    Add solution for problem `B - Internet Address` Link: https://codeforces.com/contest/245/problem/B Tags: implementation, strings, *1100 Ref: https://codeforces.com/contest/245/submission/2595539

---
+++ codeforces/245/B.java
@@ -0,0 +1,116 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		TaskB solver = new TaskB();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskB {
+	public void solve(int testNumber, InputReader in, OutputWriter out) {
+        String s=in.next();
+        StringBuilder str=new StringBuilder("");
+        int index=0;
+        if(s.startsWith("http"))
+        {
+            str.append("http://");
+               index=4;
+
+        }
+        else
+        {
+            str.append("ftp://");
+            index=3;
+        }
+        int i;
+
+        i=index;
+        int last=s.lastIndexOf("ru");
+        str.append(s.substring(index,last)+".ru");
+        last+=2;
+        if(last==s.length())
+        {
+            out.printLine(str.toString());
+            return;
+        }
+        str.append("/"+s.substring(last,s.length()));
+        out.printLine(str.toString());
+
+	}
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+
+}
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(Object...objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+
+	public void printLine(Object...objects) {
+		print(objects);
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
+}
+
