f6c5821072e5146b52214e8b0ce671c9ae57c6e6
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-11-21 21:14:00 +0530

    Add solution for problem `A - Buggy Sorting` Link: https://codeforces.com/contest/246/problem/A Tags: constructive algorithms, greedy, sortings, *900 Ref: https://codeforces.com/contest/246/submission/2612888

---
+++ codeforces/246/A.java
@@ -0,0 +1,127 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+	public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n=in.nextInt();
+        if(n<=2)
+        out.printLine(-1);
+        else 
+        {
+            for(int i=2;i<=n;i++)
+                out.print(i+" ");
+            out.printLine(1);
+        }
+        check();
+        
+	}
+    public void check()
+    {
+        int a[]={2,3,1};
+        for(int i=0;i<2;i++)
+            for(int j=i;j<2;j++)
+                if(a[j]>a[j+1])
+                {
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+        DebugUtils.print(a);
+    }
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    public int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+
+}
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(Object...objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+
+	public void printLine(Object...objects) {
+		print(objects);
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
+}
+
+class DebugUtils {
+    public static void print(Object... a)
+    {
+        boolean oj=System.getProperty("ONLINE_JUDGE")!=null;
+        if(!oj)
+            System.out.println(Arrays.deepToString(a));
+    }
+}
+
