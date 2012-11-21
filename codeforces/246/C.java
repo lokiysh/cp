1c2da5ddb462b9a1ca880441ced72373e8954e52
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-11-22 00:09:00 +0530

    Add solution for problem `C - Beauty Pageant` Link: https://codeforces.com/contest/246/problem/C Tags: brute force, constructive algorithms, greedy, *1600 Ref: https://codeforces.com/contest/246/submission/2622244

---
+++ codeforces/246/C.java
@@ -0,0 +1,118 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		TaskC solver = new TaskC();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskC {
+	public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n=in.nextInt(),k=in.nextInt(),i,a[]=new int[n];
+
+        for(i=0;i<n;i++)
+        {
+            a[i]=in.nextInt();
+        }
+        Arrays.sort(a);
+        ArrayList<Integer> b=new ArrayList<Integer>();
+        int l=n-1,m=n-1;
+        for(i=0;i<k;i++)
+        {
+            out.print(b.size()+1);
+            for(int p :b)
+                out.print(" "+p);
+            out.printLine(" "+a[l]);
+            
+            l--;
+            if(l<0)
+            {
+                b.add(a[m]);
+                m--;
+                l=m;
+            }
+        }
+	}
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    public int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+
+}
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(Object...objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+
+	public void printLine(Object...objects) {
+		print(objects);
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
+}
+
