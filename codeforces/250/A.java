4081cd0ba183e29c3c2b50b9bbe410c58ad9005d
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-11-26 19:36:00 +0530

    Add solution for problem `A - Paper Work` Link: https://codeforces.com/contest/250/problem/A Tags: greedy, *1000 Ref: https://codeforces.com/contest/250/submission/2658627

---
+++ codeforces/250/A.java
@@ -0,0 +1,115 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+	public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n=in.nextInt(),i,a[]=new int[n];
+        for(i=0;i<n;i++)
+            a[i]=in.nextInt();
+        int neg=0,p=0;
+        ArrayList<Integer> array=new ArrayList<Integer>();
+        for(i=0;i<n;i++)
+        {
+            if(a[i]<0)
+                neg++;
+            if(neg==3)
+            {
+                neg=1;
+                array.add(p);
+                p=0;
+            }
+            p++;
+        }
+        if(p>0)
+            array.add(p);
+        out.printLine(array.size());
+        for(int k :array)
+            out.print(k+" ");
+	}
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    public int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+
+}
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(Object...objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+
+	public void printLine(Object...objects) {
+		print(objects);
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
+}
+
