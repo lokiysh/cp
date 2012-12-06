28d86120fc3bd50018abb10ec8b0dcf3d81d8a5b
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2012-12-06 21:04:00 +0530

    Add solution for problem `A - Little Xor` Link: https://codeforces.com/contest/252/problem/A Tags: brute force, implementation, *1100 Ref: https://codeforces.com/contest/252/submission/2701136

---
+++ codeforces/252/A.java
@@ -0,0 +1,110 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal aka (codeKNIGHT | phantom11)
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+	public void solve(int testNumber, InputReader in, OutputWriter out) {
+        long max=-1;
+        int i,j;
+        int n=in.nextInt();
+        int a[]=new int[n];
+        for(i=0;i<n;i++)
+            a[i]=in.nextInt();
+        for(int len=1;len<=n;len++)
+        {
+            for(i=0;i<=n-len;i++)
+            {
+                long xor=0;
+                for(j=i;j<i+len;j++)
+                    xor^=a[j];
+                if(xor>max)
+                    max=xor;
+            }
+        }
+        out.printLine(max);
+	}
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    public int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+
+}
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(Object...objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+
+	public void printLine(Object...objects) {
+		print(objects);
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
+}
+
