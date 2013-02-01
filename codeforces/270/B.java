20c3bff0a38473820ce70688cbff514061b2957c
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2013-02-01 21:27:00 +0530

    Add solution for problem `B - Multithreading` Link: https://codeforces.com/contest/270/problem/B Tags: data structures, greedy, implementation, *1400 Ref: https://codeforces.com/contest/270/submission/3049201

---
+++ codeforces/270/B.java
@@ -0,0 +1,120 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		TaskB solver = new TaskB();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskB {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n=in.nextInt();
+        int a[]=new int[n],i;
+        int ans=0;
+        for(i=0;i<n;i++)
+        {
+            a[i]=in.nextInt();
+
+        }         //out.printLine(ans);
+        if(n==1)
+        {
+            out.printLine(0);
+            return;
+        }
+        boolean sta=false;
+        for(i=n-2;i>=0;i--)
+        {
+            if(a[i]>a[i+1])
+            {
+                sta=true;
+                break;
+            }
+        }
+        if(!sta)
+            out.printLine(0);
+        else
+        {
+            out.printLine(i+1);
+        }
+
+    }
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    public int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+    }
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(Object...objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+
+	public void printLine(Object...objects) {
+		print(objects);
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
+}
+
