d87ba56327c8eb0e82a5ddf0a6304e38c282ca2d
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2013-02-13 21:15:00 +0530

    Add solution for problem `B - Dima and Sequence` Link: https://codeforces.com/contest/272/problem/B Tags: implementation, math, *1400 Ref: https://codeforces.com/contest/272/submission/3113736

---
+++ codeforces/272/B.java
@@ -0,0 +1,116 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		TaskB solver = new TaskB();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskB {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n=in.nextInt(),i,a[]=new int[n];
+        //String s[]=new String[]
+        int b[]=new int[32];
+        for(i=0;i<n;i++)
+        {
+            a[i]=in.nextInt();
+            String s=Integer.toBinaryString(a[i]);
+            int len=s.length(),c=0;
+            for(int j=0;j<len;j++)
+                if(s.charAt(j)=='1')
+                    c++;
+            b[c]++;
+
+        }
+        long ans=0;
+        for(i=0;i<32;i++)
+            ans+=combo(b[i]);
+        out.printLine(ans);
+    }
+    public long combo(long x)
+    {
+
+        long y=x*(x-1);
+        y/=2;
+        return y;
+    }
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    public int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+    }
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(Object...objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+
+	public void printLine(Object...objects) {
+		print(objects);
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
+}
+
