f2d7243ce1cec8ed66bd41894c29743c3f3db669
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2013-03-17 21:19:00 +0530

    Add solution for problem `A - Cows and Sequence` Link: https://codeforces.com/contest/283/problem/A Tags: constructive algorithms, data structures, implementation, *1600 Ref: https://codeforces.com/contest/283/submission/3333177

---
+++ codeforces/283/A.java
@@ -0,0 +1,125 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		TaskA solver = new TaskA();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskA {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        long seq[]=new long[200011];
+        long uptil[]=new long[200011];
+        int i;
+        int n=in.nextInt();
+        long sum=0;
+        int num=1;
+        for(i=0;i<n;i++)
+        {
+            int t=in.nextInt();
+            if(t==1)
+            {
+                int a=in.nextInt(),x=in.nextInt();
+                sum+=x*a;
+                uptil[a-1]+=x;
+            }
+            else if(t==2)
+            {
+                int x=in.nextInt();
+                seq[num]=x;
+                sum+=x;
+                num++;
+            }
+            else
+            {
+                num--;
+                sum-=seq[num];
+                seq[num]=0;
+                sum-=uptil[num];
+                uptil[num-1]+=uptil[num];
+                uptil[num]=0;
+            }
+            out.printLine((double)sum/num);
+           
+        }
+    }
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    public int nextInt()
+    {
+        return Integer.parseInt(next());
+    }
+    }
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(Object...objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+
+	public void printLine(Object...objects) {
+		print(objects);
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
+}
+
