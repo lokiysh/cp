fbc5fed7340cab98225d04acdf49e5505747946a
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2013-09-14 21:45:00 +0530

    Add solution for problem `B - Alternating Current` Link: https://codeforces.com/contest/343/problem/B Tags: data structures, greedy, implementation, *1600 Ref: https://codeforces.com/contest/343/submission/4463934

---
+++ codeforces/343/B.java
@@ -0,0 +1,109 @@
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.util.Stack;
+import java.util.Vector;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.StringTokenizer;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ * @author Lokesh Khandelwal
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		TaskB solver = new TaskB();
+		solver.solve(1, in, out);
+		out.close();
+	}
+}
+
+class TaskB {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        String s = in.next();
+        int i, len = s.length();
+        int last = 0 , first;
+        Stack<Integer> stack = new Stack<Integer>();
+        //stack.push(0);
+        for(i=0;i<len;i++) {
+            if(stack.isEmpty()) {
+                stack.push(i);
+                continue;
+            }
+            last = stack.peek();
+            if(s.charAt(i) == s.charAt(last)) {
+                stack.pop();
+
+            }
+            else stack.push(i);
+        }
+        if(!stack.isEmpty())
+            out.printLine("No");
+        else out.printLine("Yes");
+    }
+}
+
+class InputReader
+{
+    BufferedReader in;
+    StringTokenizer tokenizer=null;
+
+    public InputReader(InputStream inputStream)
+    {
+        in=new BufferedReader(new InputStreamReader(inputStream));
+    }
+    public String next()
+    {
+        try{
+            while (tokenizer==null||!tokenizer.hasMoreTokens())
+            {
+                tokenizer=new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+    }
+
+class OutputWriter {
+	private final PrintWriter writer;
+
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+	}
+
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+
+	public void print(Object...objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+
+	public void printLine(Object...objects) {
+		print(objects);
+		writer.println();
+	}
+
+	public void close() {
+		writer.close();
+	}
+}
+
