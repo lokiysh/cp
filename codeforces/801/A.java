67d35ebf09de20cb5015d598ea8d5b344d322c75
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2017-04-16 21:14:00 +0530

    Add solution for problem `A - Vicious Keyboard` Link: https://codeforces.com/contest/801/problem/A Tags: brute force, *1100 Ref: https://codeforces.com/contest/801/submission/26418318

---
+++ codeforces/801/A.java
@@ -0,0 +1,114 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author phantom11
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskA solver = new TaskA();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskA {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            String s = in.next();
+            int n = s.length(), i;
+            if (n == 1) {
+                out.printLine(0);
+                return;
+            }
+            int ans = 0;
+            boolean done = false;
+            boolean mark[] = new boolean[n];
+            for (i = 0; i < n - 1; i++) {
+                if (s.charAt(i) == 'V' && s.charAt(i + 1) == 'K') {
+                    ans++;
+                    mark[i] = mark[i + 1] = true;
+                }
+            }
+            for (i = 0; i < n - 1; i++) {
+                if (!mark[i] && !mark[i + 1]) {
+                    if (s.charAt(i) == 'K' && s.charAt(i + 1) == 'V') {
+                        continue;
+                    }
+                    ans++;
+                    break;
+                }
+            }
+            out.printLine(ans);
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+    }
+}
+
