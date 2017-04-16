77c07276cb23bfdd1ffdd9fdae24ce131335f796
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2017-04-16 21:18:00 +0530

    Add solution for problem `B - Valued Keys` Link: https://codeforces.com/contest/801/problem/B Tags: constructive algorithms, greedy, strings, *900 Ref: https://codeforces.com/contest/801/submission/26419714

---
+++ codeforces/801/B.java
@@ -0,0 +1,101 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author phantom11
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            String x = in.next(), z = in.next();
+            StringBuilder y = new StringBuilder("");
+            int n = x.length(), i;
+            for (i = 0; i < n; i++) {
+                if (x.charAt(i) >= z.charAt(i)) {
+                    y.append(z.charAt(i));
+                } else {
+                    out.printLine(-1);
+                    return;
+                }
+            }
+            out.printLine(y.toString());
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+    }
+}
+
