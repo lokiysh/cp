2118ec573fdbccb1fb4b9f805a34d058b2d5e0ec
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2017-07-30 20:31:00 +0530

    Add solution for problem `B - The Festive Evening` Link: https://codeforces.com/contest/834/problem/B Tags: data structures, implementation, *1100 Ref: https://codeforces.com/contest/834/submission/28998911

---
+++ codeforces/834/B.java
@@ -0,0 +1,120 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author phantom11
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int n = in.nextInt(), k = in.nextInt(), i;
+            String s = in.next();
+            int opened[] = new int[26], closed[] = new int[26];
+            Arrays.fill(opened, -1);
+            Arrays.fill(closed, -1);
+            for (i = 0; i < n; i++) {
+                if (opened[s.charAt(i) - 'A'] == -1) {
+                    opened[s.charAt(i) - 'A'] = i;
+                }
+                closed[s.charAt(i) - 'A'] = i;
+            }
+            int o = 0;
+            for (i = 0; i < n; i++) {
+                if (opened[s.charAt(i) - 'A'] == i) {
+                    o++;
+                }
+                if (o > k) {
+                    out.printLine("YES");
+                    return;
+                }
+                if (closed[s.charAt(i) - 'A'] == i) {
+                    o--;
+                }
+
+            }
+            out.printLine("NO");
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+}
+
