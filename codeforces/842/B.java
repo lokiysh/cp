5ca42aa8cccadb70d2401681bd6348d3ee399219
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2017-08-29 21:04:00 +0530

    Add solution for problem `B - Gleb And Pizza` Link: https://codeforces.com/contest/842/problem/B Tags: geometry, *1100 Ref: https://codeforces.com/contest/842/submission/29880134

---
+++ codeforces/842/B.java
@@ -0,0 +1,109 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author phantom11
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int r = in.nextInt(), d = in.nextInt();
+            int n = in.nextInt(), i, ans = 0;
+            for (i = 0; i < n; i++) {
+                int x = in.nextInt(), y = in.nextInt();
+                int ri = in.nextInt();
+                int distance = x * x + y * y;
+                //check if it lies completely in the circle
+                boolean p = (distance <= (r - ri) * (r - ri));
+                //check if it lies outside of inner circle
+                boolean q = (distance >= (r - d + ri) * (r - d + ri));
+                //DebugUtils.debug(p, q);
+                if (p && q) {
+                    ans++;
+                }
+            }
+            out.printLine(ans);
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
