fff4ffc42e54bc164083ba87688974962a29cf55
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2017-09-17 18:52:00 +0530

    Add solution for problem `A - k-rounding` Link: https://codeforces.com/contest/861/problem/A Tags: math, number theory, *1100 Ref: https://codeforces.com/contest/861/submission/30425240

---
+++ codeforces/861/A.java
@@ -0,0 +1,116 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author phantom11
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskA solver = new TaskA();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskA {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            long n = in.nextInt(), k = in.nextInt();
+
+            long copyN = n;
+            int numTwo = 0, numFive = 0;
+            while (copyN > 0 && copyN % 2 == 0) {
+                numTwo++;
+                copyN /= 2;
+            }
+            copyN = n;
+            while (copyN > 0 && copyN % 5 == 0) {
+                numFive++;
+                copyN /= 5;
+            }
+            while (k > Math.min(numFive, numTwo)) {
+                if (numFive < numTwo) {
+                    numFive++;
+                    n *= 5L;
+                } else {
+                    numTwo++;
+                    n *= 2L;
+                }
+            }
+            out.printLine(n);
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
