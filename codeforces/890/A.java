8be690ba7ee7db1fdf8fa0662353a19642ffa283
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2017-11-12 21:40:00 +0530

    Add solution for problem `A - ACM ICPC` Link: https://codeforces.com/contest/890/problem/A Tags: brute force, *1000 Ref: https://codeforces.com/contest/890/submission/32249110

---
+++ codeforces/890/A.java
@@ -0,0 +1,123 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author phantom11
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskA solver = new TaskA();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskA {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int a[] = new int[6];
+            int i, total = 0;
+            for (i = 0; i < 6; i++) {
+                a[i] = in.nextInt();
+                total += a[i];
+            }
+            Arrays.sort(a);
+            int A = 0, B = 0;
+            for (i = (1 << 6) - 1; i > 0; i--) {
+                String s = Integer.toBinaryString(i);
+                while (s.length() < 6) {
+                    s = "0" + s;
+                }
+                int count = 0;
+                A = 0;
+                for (int j = 0; j < 6; j++) {
+                    if (s.charAt(j) == '1') {
+                        count++;
+                        A += a[j];
+                    }
+                }
+                if (count == 3) {
+                    if (total - A == A) {
+                        out.printLine("YES");
+                        return;
+                    }
+                }
+            }
+            out.printLine("NO");
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+}
+
