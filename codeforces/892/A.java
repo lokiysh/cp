a9d3a78260575c80738f22e4f0a822c898bdef27
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2017-11-17 20:09:00 +0530

    Add solution for problem `A - Greed` Link: https://codeforces.com/contest/892/problem/A Tags: greedy, implementation, *900 Ref: https://codeforces.com/contest/892/submission/32382355

---
+++ codeforces/892/A.java
@@ -0,0 +1,121 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author phantom11
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskA solver = new TaskA();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskA {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            Integer A[] = in.nextIntArray(N);
+            Integer B[] = in.nextIntArray(N);
+            if (N <= 2) {
+                out.printLine("YES");
+                return;
+            }
+            Arrays.sort(B);
+            long sum = 0;
+            for (int i = 0; i < N; i++) {
+                sum += A[i];
+            }
+            long cap = B[N - 1];
+            cap += B[N - 2];
+            if (sum <= cap) {
+                out.printLine("YES");
+            } else {
+                out.printLine("NO");
+            }
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public Integer[] nextIntArray(int size) {
+            Integer array[] = new Integer[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = nextInt();
+            }
+            return array;
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+}
+
