be2717c34fe96808b6e5c9d94540c34334a2eff5
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2017-11-19 21:52:00 +0530

    Add solution for problem `B - Ralph And His Magic Field` Link: https://codeforces.com/contest/894/problem/B Tags: combinatorics, constructive algorithms, math, number theory, *1800 Ref: https://codeforces.com/contest/894/submission/32476697

---
+++ codeforces/894/B.java
@@ -0,0 +1,125 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author phantom11
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            long N = in.nextLong(), M = in.nextLong(), K = in.nextInt();
+            if ((N + M) % 2 == 1 && K == -1) {
+                out.printLine(0);
+            } else {
+                N -= 1;
+                M -= 1;
+                long MOD = (long) 1e9 + 7;
+                N %= (MOD - 1);
+                M %= (MOD - 1);
+                long ans = IntegerUtils.power(2, N * M, MOD);
+                out.printLine(ans);
+            }
+
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class IntegerUtils {
+        public static long power(long x, long y, long mod) {
+            if (y == 0) return 1;
+            if (y % 2 == 0) {
+                long sub = power(x, y / 2, mod);
+                return (sub * sub) % mod;
+            } else {
+                long sub = power(x, y - 1, mod);
+                x %= mod;
+                return (sub * x) % mod;
+            }
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+    }
+}
+
