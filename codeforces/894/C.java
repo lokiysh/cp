a767376190d8eb2d7b1cc70a0ab6213fe33a278d
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2017-11-19 21:53:00 +0530

    Add solution for problem `C - Marco and GCD Sequence` Link: https://codeforces.com/contest/894/problem/C Tags: constructive algorithms, math, *1900 Ref: https://codeforces.com/contest/894/submission/32476754

---
+++ codeforces/894/C.java
@@ -0,0 +1,115 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ *
+ * @author phantom11
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskC solver = new TaskC();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskC {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt(), i, j;
+            Integer a[] = in.nextIntArray(N);
+            for (i = 1; i < N; i++) {
+                if (a[i] % a[0] > 0) {
+                    out.printLine(-1);
+                    return;
+                }
+            }
+            out.printLine(2 * N - 1);
+            out.print(a[0] + " ");
+            for (i = 1; i < N; i++) {
+                out.print(a[i] + " " + a[0] + " ");
+            }
+            out.printLine();
+        }
+
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public Integer[] nextIntArray(int size) {
+            Integer array[] = new Integer[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = nextInt();
+            }
+            return array;
+        }
+
+    }
+}
+
