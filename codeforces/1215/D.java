6fbf227d8ba7679db9ae15ec80308926221d2e3d
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2019-09-15 20:30:00 +0530

    Add solution for problem `D - Ticket Game` Link: https://codeforces.com/contest/1215/problem/D Tags: games, greedy, math, *1700 Ref: https://codeforces.com/contest/1215/submission/60639470

---
+++ codeforces/1215/D.java
@@ -0,0 +1,118 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskD solver = new TaskD();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskD {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            char a[] = in.next().toCharArray();
+            int left = 0, right = 0;
+            int leftQ = 0, rightQ = 0;
+            for (int i = 0; i < N; i++) {
+                if (i < N / 2) {
+                    if (a[i] == '?') {
+                        leftQ++;
+                    } else {
+                        left += a[i] - '0';
+                    }
+                } else {
+                    if (a[i] == '?') {
+                        rightQ++;
+                    } else {
+                        right += a[i] - '0';
+                    }
+                }
+            }
+            left += leftQ / 2 * 9;
+            right += rightQ / 2 * 9;
+            if (left == right) {
+                out.printLine("Bicarp");
+            } else {
+                out.printLine("Monocarp");
+            }
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
