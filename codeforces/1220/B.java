f63ed889f39ea804c4f309ccb8e1704f89c5fb49
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2019-09-18 21:52:00 +0530

    Add solution for problem `B - Multiplication Table` Link: https://codeforces.com/contest/1220/problem/B Tags: math, number theory, *1300 Ref: https://codeforces.com/contest/1220/submission/60788276

---
+++ codeforces/1220/B.java
@@ -0,0 +1,109 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            long a[][] = new long[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    a[i][j] = in.nextInt();
+                }
+            }
+            a[1][1] = (long) Math.sqrt(a[0][1] * a[1][2] / a[0][2]);
+//        long p = (long)Math.sqrt(a[1][1]);
+            for (int i = 0; i < N; i++) {
+                if (i == 1) continue;
+                a[i][i] = a[i][1] / a[1][1];
+            }
+            for (int i = 0; i < N; i++) {
+                out.print(a[i][i] + " ");
+            }
+            out.printLine();
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
