b223df4ca6ee377446e13286b18e02fb23ca5b3f
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2019-10-08 20:27:00 +0530

    Add solution for problem `B - Kill `Em All` Link: https://codeforces.com/contest/1238/problem/B Tags: greedy, sortings, *1300 Ref: https://codeforces.com/contest/1238/submission/62128805

---
+++ codeforces/1238/B.java
@@ -0,0 +1,121 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int Q = in.nextInt();
+            while (Q-- > 0) {
+                int N = in.nextInt(), r = in.nextInt();
+                Integer a[] = in.nextIntArray(N);
+                Arrays.sort(a);
+                int ans = 0, lastSeen = Constants.INT_INFINITY;
+                for (int i = N - 1; i >= 0; i--) {
+                    if (a[i] - ans * r <= 0 || a[i] >= lastSeen) {
+                        continue;
+                    }
+                    lastSeen = a[i];
+                    ans++;
+                }
+                out.printLine(ans);
+            }
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public Integer[] nextIntArray(int size) {
+            Integer array[] = new Integer[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = nextInt();
+            }
+            return array;
+        }
+
+    }
+
+    static class Constants {
+        public static final Integer INT_INFINITY = (int) 2e9;
+
+    }
+}
+
