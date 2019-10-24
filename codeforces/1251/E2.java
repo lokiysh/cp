8022f808859053f9079ce3cafefd2dc2b95e401a
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2019-10-25 02:47:00 +0530

    Add solution for problem `E2 - Voting (Hard Version)` Link: https://codeforces.com/contest/1251/problem/E2 Tags: binary search, data structures, greedy, *2400 Ref: https://codeforces.com/contest/1251/submission/63350115

---
+++ codeforces/1251/E2.java
@@ -0,0 +1,117 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.PriorityQueue;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskE solver = new TaskE();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskE {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            PriorityQueue<Integer> queue = new PriorityQueue<>();
+            List<Integer>[] votesRequired = new List[N + 1];
+            for (int i = 0; i < N; i++) {
+                votesRequired[i] = new ArrayList();
+            }
+            for (int i = 0; i < N; i++) {
+                int m = in.nextInt(), p = in.nextInt();
+                votesRequired[m].add(p);
+            }
+            long ans = 0;
+            for (int i = N - 1; i > 0; i--) {
+                for (int price : votesRequired[i]) {
+                    queue.add(price);
+                }
+                while (queue.size() > N - i) {
+                    ans += queue.poll();
+                }
+            }
+            out.printLine(ans);
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
