d3c226bda6b9f2e0ca6d2563dc212627aa1b7c8c
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2019-10-24 22:59:00 +0530

    Add solution for problem `A - Broken Keyboard` Link: https://codeforces.com/contest/1251/problem/A Tags: brute force, strings, two pointers, *1000 Ref: https://codeforces.com/contest/1251/submission/63340304

---
+++ codeforces/1251/A.java
@@ -0,0 +1,115 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskA solver = new TaskA();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskA {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            boolean single[] = new boolean[26];
+            String s = in.next();
+            int N = s.length();
+            if (N == 1) {
+                out.printLine(s);
+                return;
+            }
+            char ch[] = s.toCharArray();
+            for (int i = 1; i < N; i++) {
+                if (ch[i] == ch[i - 1]) {
+                    ch[i] = ch[i - 1] = ' ';
+                }
+            }
+            for (int i = 0; i < N; i++) {
+                if (ch[i] != ' ') {
+                    single[ch[i] - 'a'] = true;
+                }
+            }
+            String ans = "";
+            for (int i = 0; i < 26; i++) {
+                if (single[i]) {
+                    ans += (char) (i + 'a');
+                }
+            }
+            out.printLine(ans);
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+    }
+}
+
