e4b3c5125ecff8766851a11c4a6884c054a050ab
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2019-10-26 16:50:00 +0530

    Add solution for problem `B1 - TV Subscriptions (Easy Version)` Link: https://codeforces.com/contest/1247/problem/B1 Tags: implementation, *1000 Ref: https://codeforces.com/contest/1247/submission/63448858

---
+++ codeforces/1247/B1.java
@@ -0,0 +1,127 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.util.Hashtable;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt(), K = in.nextInt(), D = in.nextInt();
+            Hashtable<Integer, Integer> table = new Hashtable<>();
+            Integer a[] = in.nextIntArray(N);
+            for (int i = 0; i < D; i++) {
+                if (table.containsKey(a[i])) {
+                    table.put(a[i], table.get(a[i]) + 1);
+                } else {
+                    table.put(a[i], 1);
+                }
+            }
+            int ans = table.size();
+            for (int i = D; i < N; i++) {
+                table.put(a[i - D], table.get(a[i - D]) - 1);
+                if (table.get(a[i - D]) == 0) {
+                    table.remove(a[i - D]);
+                }
+                if (table.containsKey(a[i])) {
+                    table.put(a[i], table.get(a[i]) + 1);
+                } else {
+                    table.put(a[i], 1);
+                }
+                ans = Math.min(ans, table.size());
+            }
+            out.printLine(ans);
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public Integer[] nextIntArray(int size) {
+            Integer array[] = new Integer[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = nextInt();
+            }
+            return array;
+        }
+
+    }
+}
+
