644f419697952d2a3d6da13895875ff54dc2703e
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2019-11-06 20:47:00 +0530

    Add solution for problem `B1 - Character Swap (Easy Version)` Link: https://codeforces.com/contest/1243/problem/B1 Tags: strings, *1000 Ref: https://codeforces.com/contest/1243/submission/64379009

---
+++ codeforces/1243/B1.java
@@ -0,0 +1,116 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB1 solver = new TaskB1();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskB1 {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            String s = in.next(), t = in.next();
+            int diff = 0;
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                if (s.charAt(i) != t.charAt(i)) {
+                    list.add(i);
+                    diff++;
+                }
+            }
+            String ans = "NO";
+            if (diff == 0) {
+                ans = "YES";
+            } else if (diff == 2) {
+//            DebugUtils.debug(list);
+                if (s.charAt(list.get(0)) == s.charAt(list.get(1)) && t.charAt(list.get(1)) == t.charAt(list.get(0))) {
+                    ans = "YES";
+                }
+            }
+            out.printLine(ans);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
