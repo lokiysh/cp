ed0533181ea31b1f1a1ed60a93ae0cdd8225ae5e
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-03-01 19:30:00 +0530

    Add solution for problem `B - Journey Planning` Link: https://codeforces.com/contest/1321/problem/B Tags: data structures, sortings, *1400 Ref: https://codeforces.com/contest/1321/submission/72183433

---
+++ codeforces/1321/B.java
@@ -0,0 +1,113 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.util.Hashtable;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            int a[] = new int[N];
+            int b[] = new int[N];
+            for (int i = 0; i < N; i++) {
+                a[i] = in.nextInt();
+                b[i] = a[i] - (i + 1);
+            }
+            Hashtable<Integer, Long> table = new Hashtable<>();
+            for (int i = 0; i < N; i++) {
+                if (table.containsKey(b[i])) {
+                    table.put(b[i], table.get(b[i]) + a[i]);
+                } else {
+                    table.put(b[i], (long) a[i]);
+                }
+            }
+            long ans = -1;
+            for (long values : table.values()) {
+                ans = Math.max(ans, values);
+            }
+            out.printLine(ans);
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
