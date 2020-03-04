dfba66647e29a151656cf504fc3a1796d566db92
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-03-04 20:19:00 +0530

    Add solution for problem `B - String Modification` Link: https://codeforces.com/contest/1316/problem/B Tags: brute force, constructive algorithms, implementation, sortings, strings, *1400 Ref: https://codeforces.com/contest/1316/submission/72429738

---
+++ codeforces/1316/B.java
@@ -0,0 +1,113 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            String s = in.next();
+            String minString = s;
+            int kMinimum = 1;
+            for (int i = 2; i <= N; i++) {
+                String newString = s.substring(i - 1);
+                int p = N - i + 1;
+                if (p % 2 == 0) {
+                    newString += s.substring(0, i - 1);
+                } else {
+                    newString += new StringBuilder(s.substring(0, i - 1)).reverse().toString();
+                }
+                if (newString.compareTo(minString) < 0) {
+                    minString = newString;
+                    kMinimum = i;
+                }
+            }
+            out.printLine(minString);
+            out.printLine(kMinimum);
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
