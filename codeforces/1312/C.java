bc04915594eb253cffe5b05a28f324c4a0c9874b
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-03-10 15:49:00 +0530

    Add solution for problem `C - Adding Powers` Link: https://codeforces.com/contest/1312/problem/C Tags: bitmasks, greedy, implementation, math, number theory, ternary search, *1400 Ref: https://codeforces.com/contest/1312/submission/72884508

---
+++ codeforces/1312/C.java
@@ -0,0 +1,123 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskC solver = new TaskC();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskC {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            int K = in.nextInt();
+            long a[] = new long[N];
+            int count[] = new int[100];
+
+            for (int i = 0; i < N; i++) {
+                a[i] = in.nextLong();
+                int index = 0;
+                while (a[i] > 0) {
+                    long rem = a[i] % K;
+                    if (rem > 0) {
+                        count[index] += rem;
+                    }
+                    a[i] /= K;
+                    index++;
+                }
+            }
+            for (int i = 0; i < 100; i++) {
+                if (count[i] > 1) {
+                    out.printLine("NO");
+                    return;
+                }
+            }
+            out.printLine("YES");
+
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+    }
+}
+
