586c672ad909f316242ca95625258cb60e813f07
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-04-03 21:12:00 +0530

    Add solution for problem `A - Dreamoon and Ranking Collection` Link: https://codeforces.com/contest/1330/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/1330/submission/75389845

---
+++ codeforces/1330/A.java
@@ -0,0 +1,108 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+*/public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		InputReader in = new InputReader(inputStream);
+		OutputWriter out = new OutputWriter(outputStream);
+		TaskA solver = new TaskA();
+		int testCount = Integer.parseInt(in.next());
+		for (int i = 1; i <= testCount; i++)
+			solver.solve(i, in, out);
+		out.close();
+	}
+static class TaskA   {
+public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int N = in.nextInt(), X = in.nextInt();
+        boolean a[] = new boolean[300];
+        for (int i = 0; i < N; i++) {
+            a[in.nextInt()] = true;
+        }
+        int ans = 0;
+        for (int i = 1; i < 300; i++) {
+            if (!a[i] && X > 0) {
+                a[i] = true;
+                X--;
+            }
+            if (a[i]) {
+                ans = i;
+            } else {
+                break;
+            }
+        }
+        out.printLine(ans);
+    }
+
+}
+static class InputReader   {
+BufferedReader in;
+StringTokenizer tokenizer = null;
+
+public InputReader(InputStream inputStream) {
+        in = new BufferedReader(new InputStreamReader(inputStream));
+    }
+
+public String next() {
+        try {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                tokenizer = new StringTokenizer(in.readLine());
+            }
+            return tokenizer.nextToken();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+}
+static class OutputWriter   {
+PrintWriter writer;
+
+public OutputWriter(OutputStream outputStream) {
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+    }
+
+public OutputWriter(Writer writer) {
+        this.writer = new PrintWriter(writer);
+    }
+
+public void print(Object... objects) {
+        for(int i = 0; i < objects.length; i++) {
+            if (i != 0) {
+                writer.print(' ');
+            }
+            writer.print(objects[i]);
+        }
+    }
+
+public void printLine(Object... objects) {
+        print(objects);
+        writer.println();
+    }
+
+public void close() {
+        writer.close();
+    }
+
+}
+}
+
