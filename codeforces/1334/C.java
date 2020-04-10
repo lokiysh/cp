2fabd138861546a5e393a6f48154fcad8fe95c45
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-04-10 20:47:00 +0530

    Add solution for problem `C - Circle of Monsters` Link: https://codeforces.com/contest/1334/problem/C Tags: brute force, constructive algorithms, greedy, math, *1600 Ref: https://codeforces.com/contest/1334/submission/76141342

---
+++ codeforces/1334/C.java
@@ -0,0 +1,123 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskC solver = new TaskC();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskC {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            long a[] = new long[N], b[] = new long[N];
+            long ans = 0, min = (long) 1e18;
+            for (int i = 0; i < N; i++) {
+                a[i] = in.nextLong();
+                b[i] = in.nextLong();
+
+            }
+            if (N == 1) {
+                out.printLine(a[0]);
+                return;
+            }
+            for (int i = 0; i < N; i++) {
+                long p = a[i] - b[(i - 1 + N) % N];
+                if (p > 0) {
+                    ans += p;
+                    a[i] -= p;
+                }
+            }
+            for (int i = 0; i < N; i++) {
+                min = Math.min(min, a[i]);
+            }
+            ans += min;
+            out.printLine(ans);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+    }
+}
+
