37d1096bfd135fbf1dd1d2c04f673b6964df0531
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-05-17 15:11:00 +0530

    Add solution for problem `B - Ternary String` Link: https://codeforces.com/contest/1354/problem/B Tags: binary search, dp, implementation, two pointers, *1200 Ref: https://codeforces.com/contest/1354/submission/80468626

---
+++ codeforces/1354/B.java
@@ -0,0 +1,115 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            String s = in.next();
+            int N = s.length();
+            int last[] = new int[4];
+            Arrays.fill(last, -1);
+            int ans = Integer.MAX_VALUE;
+            for (int i = 0; i < N; i++) {
+                int num = s.charAt(i) - '0';
+                int minPrev = Integer.MAX_VALUE;
+                int count = 0;
+                for (int j = 1; j <= 3; j++) {
+                    if (j != num && last[j] != -1) {
+                        count++;
+                        minPrev = Math.min(minPrev, last[j]);
+                    }
+                }
+                if (count == 2) {
+                    ans = Math.min(ans, i - minPrev + 1);
+                }
+                last[num] = i;
+            }
+            if (ans == Integer.MAX_VALUE) {
+                ans = 0;
+            }
+            out.printLine(ans);
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+    }
+}
+
