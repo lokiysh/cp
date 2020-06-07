797c956cb5bec98b1735f11578aff5084f5aa869
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-06-07 20:24:00 +0530

    Add solution for problem `C - Rotation Matching` Link: https://codeforces.com/contest/1365/problem/C Tags: constructive algorithms, data structures, greedy, implementation, *1400 Ref: https://codeforces.com/contest/1365/submission/82801978

---
+++ codeforces/1365/C.java
@@ -0,0 +1,115 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskC solver = new TaskC();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskC {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            int a[] = new int[N], b[] = new int[N];
+            int posA[] = new int[N], posB[] = new int[N];
+            for (int i = 0; i < N; i++) {
+                a[i] = in.nextInt() - 1;
+                posA[a[i]] = i - a[i];
+            }
+            for (int i = 0; i < N; i++) {
+                b[i] = in.nextInt() - 1;
+                posB[b[i]] = i - b[i];
+            }
+            int diff[] = new int[N];
+            int count[] = new int[N];
+            int ans = 0;
+            for (int i = 0; i < N; i++) {
+                diff[i] = (posA[i] - posB[i] + N) % N;
+                count[diff[i]]++;
+                if (count[diff[i]] > ans) {
+                    ans = count[diff[i]];
+                }
+            }
+            out.printLine(ans);
+
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
