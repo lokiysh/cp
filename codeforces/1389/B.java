772a5d2c1b7bf824c7b1be227c672f6fb6779fd5
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-07-29 22:17:00 +0530

    Add solution for problem `B - Array Walk` Link: https://codeforces.com/contest/1389/problem/B Tags: brute force, dp, greedy, *1600 Ref: https://codeforces.com/contest/1389/submission/88368975

---
+++ codeforces/1389/B.java
@@ -0,0 +1,118 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt(), K = in.nextInt(), Z = in.nextInt();
+            int a[] = new int[N + 1];
+            for (int i = 0; i < N; i++) {
+                a[i] = in.nextInt();
+            }
+            a[N] = Integer.MIN_VALUE;
+            long ans = 0;
+
+            for (int z = 0; z <= Z; z++) {
+                int max = 0;
+                int left = K - 2 * z;
+                if (left < 0) {
+                    continue;
+                }
+                for (int i = 0; i <= left; i++) {
+                    max = Math.max(max, a[i] + a[i + 1]);
+                }
+                long current = 0;
+                for (int i = 0; i <= left; i++) {
+                    current += a[i];
+                }
+                ans = Math.max(ans, current + z * max);
+            }
+            out.printLine(ans);
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
+
