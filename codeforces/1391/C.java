35bc7cdd13a6bedbc75ee22458f764e48b6f8b84
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-08-09 20:49:00 +0530

    Add solution for problem `C - Cyclic Permutations ` Link: https://codeforces.com/contest/1391/problem/C Tags: combinatorics, dp, graphs, math, *1500 Ref: https://codeforces.com/contest/1391/submission/89435787

---
+++ codeforces/1391/C.java
@@ -0,0 +1,108 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskC solver = new TaskC();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskC {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            long MOD = (int) 1e9 + 7;
+            long fact[] = new long[N + 1];
+            fact[0] = 1;
+            for (int i = 1; i <= N; i++) {
+                fact[i] = (fact[i - 1] * i) % MOD;
+            }
+            long prev[] = new long[N + 1];
+            prev[3] = 2;
+            for (int i = 4; i <= N; i++) {
+                prev[i] = fact[i - 1] * (i - 2);
+                prev[i] %= MOD;
+                prev[i] += prev[i - 1] * 2;
+                prev[i] %= MOD;
+            }
+            out.printLine(prev[N]);
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+}
+
