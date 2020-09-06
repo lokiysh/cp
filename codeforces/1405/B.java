500a306d12dda728b47b0c54888be81b774bb7f9
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-09-06 20:29:00 +0530

    Add solution for problem `B - Array Cancellation` Link: https://codeforces.com/contest/1405/problem/B Tags: constructive algorithms, implementation, *1000 Ref: https://codeforces.com/contest/1405/submission/92040751

---
+++ codeforces/1405/B.java
@@ -0,0 +1,126 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.util.Collection;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.util.Queue;
+import java.io.BufferedReader;
+import java.util.LinkedList;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt();
+            int a[] = new int[N];
+            for (int i = 0; i < N; i++) {
+                a[i] = in.nextInt();
+            }
+            long answer = 0;
+            Queue<Integer> queue = new LinkedList<Integer>();
+            for (int i = 0; i < N; i++) {
+                if (a[i] > 0) {
+                    queue.add(i);
+                } else if (a[i] < 0) {
+                    while (!queue.isEmpty()) {
+                        int x = queue.poll();
+                        int y = Math.min(a[x], Math.abs(a[i]));
+                        a[x] -= y;
+                        a[i] += y;
+                        if (a[x] != 0) {
+                            queue.add(x);
+                        }
+                        if (a[i] == 0) {
+                            break;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < N; i++) {
+                answer += Math.abs(a[i]);
+            }
+            out.printLine(answer / 2);
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+}
+
