4650f4d66892544f771088db1b49c3593aeba244
Lokesh Khandelwal <lokesh.khandelwal92@gmail.com>
2020-12-06 20:46:00 +0530

    Add solution for problem `B - Balls of Steel` Link: https://codeforces.com/contest/1450/problem/B Tags: brute force, geometry, greedy, *1000 Ref: https://codeforces.com/contest/1450/submission/100549220

---
+++ codeforces/1450/B.java
@@ -0,0 +1,128 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        OutputWriter out = new OutputWriter(outputStream);
+        TaskB solver = new TaskB();
+        int testCount = Integer.parseInt(in.next());
+        for (int i = 1; i <= testCount; i++)
+            solver.solve(i, in, out);
+        out.close();
+    }
+
+    static class TaskB {
+        public void solve(int testNumber, InputReader in, OutputWriter out) {
+            int N = in.nextInt(), K = in.nextInt();
+            Points p[] = new Points[N];
+            for (int i = 0; i < N; i++) {
+                p[i] = new Points(in.nextInt(), in.nextInt());
+            }
+            int ans = Integer.MAX_VALUE;
+            for (int i = 0; i < N; i++) {
+                int c = 0;
+                for (int j = 0; j < N; j++) {
+                    int d = Math.abs(p[i].x - p[j].x) + Math.abs(p[i].y - p[j].y);
+                    if (d <= K) {
+                        c++;
+                    }
+                }
+                if (c == N) {
+                    out.printLine(1);
+                    return;
+                }
+            }
+
+            out.printLine(-1);
+        }
+
+        class Points {
+            int x;
+            int y;
+            int d;
+
+            public Points(int x, int y) {
+                this.x = x;
+                this.y = y;
+                this.d = x + y;
+            }
+
+        }
+
+    }
+
+    static class InputReader {
+        BufferedReader in;
+        StringTokenizer tokenizer = null;
+
+        public InputReader(InputStream inputStream) {
+            in = new BufferedReader(new InputStreamReader(inputStream));
+        }
+
+        public String next() {
+            try {
+                while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                    tokenizer = new StringTokenizer(in.readLine());
+                }
+                return tokenizer.nextToken();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+
+    static class OutputWriter {
+        PrintWriter writer;
+
+        public OutputWriter(OutputStream outputStream) {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+        }
+
+        public OutputWriter(Writer writer) {
+            this.writer = new PrintWriter(writer);
+        }
+
+        public void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0) {
+                    writer.print(' ');
+                }
+                writer.print(objects[i]);
+            }
+        }
+
+        public void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        public void close() {
+            writer.close();
+        }
+
+    }
+}
+
